d6228c5af0d531b74cc15192d50a585e191169cb
Nikolay Epifanov <nik.epifanov@gmail.com>
2021-06-30 13:39:58 +0300

    Remove unused code

--- test/ecto_squash_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EctoSquashTest do
-  use ExUnit.Case
-  doctest EctoSquash
-
-  test "greets the world" do
-    assert EctoSquash.hello() == :world
-  end
-end
--- test/support/repo.exs
+++
@@ -1,7 +0,0 @@
-defmodule Support.Repo do
-  @moduledoc false
-
-  use Ecto.Repo,
-    otp_app: :ecto_squash,
-    adapter: Ecto.Adapters.Postgres
-end
